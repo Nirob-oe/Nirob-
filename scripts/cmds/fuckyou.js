6cdf76756067d25ad345bb35e8c239ae569bc795
Nirob-oe <148666328+Nirob-oe@users.noreply.github.com>
2025-04-04 15:11:25 +0600

    Update fuckyou.js

--- scripts/cmds/fuckyou.js
+++ scripts/cmds/fuckyou.js
@@ -15,7 +15,7 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "fuck") {
  return message.reply({
  body: "Fuck you too🖕",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/9bNeakd.gif")
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/g0NycJs.gif")
  });
  }
  }
