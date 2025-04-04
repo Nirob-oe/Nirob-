3e4e8c7a26ce8cc49018d8d45d132322317c91df
Nirob-oe <148666328+Nirob-oe@users.noreply.github.com>
2025-04-05 00:26:30 +0600

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -15,16 +15,16 @@ onChat: async function({ event, message, getLang }) {
 if (event.body && event.body.toLowerCase() === "prefix") {
 return message.reply({
 body: `
-‣ 𝐆𝐥𝐨𝐛𝐚𝐥 𝐩𝐫𝐞𝐟𝐢𝐱:. 
-‣𝐘𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 𝐩𝐫𝐞𝐟𝐢𝐱:. 
+‣ 𝐆𝐥𝐨𝐛𝐚𝐥 𝐩𝐫𝐞𝐟𝐢𝐱: /
+‣𝐘𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 𝐩𝐫𝐞𝐟𝐢𝐱: /
 
 ‣ 𝐀𝐝𝐦𝐢𝐧 
-‣ kakashi/nirob
+‣ NI R OB
 
 ‣ 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 ⓕ
-‣KAKASHI HATAKE 
+‣KAKASHÎ HATAKE / Yung Zhen
 \n\n\n  `,
-attachment: await global.utils.getStreamFromURL("https://i.imgur.com/yCkColX.jpeg")
+attachment: await global.utils.getStreamFromURL("https://i.imgur.com/SEb4SxR.jpeg")
 });
 }
 }
